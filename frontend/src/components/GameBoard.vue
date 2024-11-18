507a8ede36768d0ac1486f2214739f4a8bd89150
Trueman Phamdo <trueman@phamdo.com>
2024-11-18 13:13:08 -0500

    Add styling/logic for placing ship

--- frontend/src/components/GameBoard.vue
+++ frontend/src/components/GameBoard.vue
@@ -1,18 +1,18 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { io } from 'socket.io-client'
-import { onMounted } from 'vue';
+import { onMounted } from 'vue'
 
 onMounted(() => {
   document.addEventListener('keyup', (event) => {
     if (event.code === 'Space' && onBoard) {
       //change direction
-      onHoverLeave(lastHoveredCoords);
-      direction = direction === Direction.DOWN ? Direction.RIGHT : Direction.DOWN;
-      onHover(lastHoveredCoords);
+      onHoverLeave(lastHoveredCoords)
+      direction = direction === Direction.DOWN ? Direction.RIGHT : Direction.DOWN
+      onHover(lastHoveredCoords)
     }
-  });
-});
+  })
+})
 
 enum CellValue {
   EMPTY = 'E',
@@ -29,11 +29,48 @@ const board = ref(
     .fill(null)
     .map(() => Array(COLS).fill(CellValue.EMPTY)),
 )
+
+class Ship {
+  readonly len: number;
+  constructor(len: number) {
+    this.len = len;
+  }
+}
+
+class Ships {
+  private static _SHIPS: Ship[] = [
+    new Ship(5),
+    new Ship(4),
+    new Ship(3),
+    new Ship(3),
+    new Ship(2),
+  ];
+  private static _idx = 0;
+
+  public static get curShip(): Ship | null {
+    console.log('inside');
+    console.log(this._SHIPS);
+    console.log(this._SHIPS[0]);
+    console.log(this._idx);
+    if (this._idx >= this._SHIPS.length) return null;
+    return this._SHIPS[this._idx];
+  }
+
+  public static next() {
+    ++this._idx;
+  }
+
+  // static class
+  private constructor() { }
+}
+
+
+console.log(Ships.curShip);
 const socket = io('http://localhost:3000/')
 const gameStarted = false
 let direction = Direction.DOWN
-let onBoard = false;
-let lastHoveredCoords: Coordinate | null = null;
+let onBoard = false
+let lastHoveredCoords: Coordinate | null = null
 
 interface Coordinate {
   x: number
@@ -56,9 +93,8 @@ function isValidPlacement(placement: Placement): boolean {
 
   let valid = true
   for (let i = 0; i < shipLength; ++i) {
-    if (direction === Direction.DOWN) valid &= isEmptyCoordinate({ x: start.x, y: start.y + i })
-    // Direction.RIGHT
-    else valid &= isEmptyCoordinate({ x: start.x + i, y: start.y })
+    if (direction === Direction.DOWN) valid &= isValidEmptyCoordinate({ x: start.x, y: start.y + i })
+    else valid &= isValidEmptyCoordinate({ x: start.x + i, y: start.y })
   }
   return valid
 }
@@ -67,24 +103,33 @@ function isValidCoordinate({ x, y }: Coordinate) {
   return x >= 0 && y >= 0 && x < COLS && y < ROWS
 }
 
-function isEmptyCoordinate({ x, y }: Coordinate) {
+function isValidEmptyCoordinate({ x, y }: Coordinate) {
   if (!isValidCoordinate({ x, y })) return false
   const cellValue = board.value[y][x]
   return cellValue === CellValue.EMPTY || cellValue === CellValue.HOVER
 }
 
-function onCellClick({ x, y }: Coordinate) {
-  if (!gameStarted) {
-    socket.emit('place ship', { coord: { x, y } })
+function placeShip({ x, y }: Coordinate) {
+  if (!gameStarted && Ships.curShip) {
+    if (isValidPlacement({ start: { x, y }, shipLength: Ships.curShip.len, direction })) {
+      socket.emit('place ship', { coord: { x, y } })
+
+      for (let i = 0; i < Ships.curShip.len; ++i) {
+        if (direction === Direction.DOWN) board.value[y + i][x] = CellValue.SHIP;
+        else board.value[y][x + i] = CellValue.SHIP;
+      }
+
+      console.log('next');
+      Ships.next();
+    }
   }
 }
 
 function onHover({ x, y }: Coordinate) {
-  if (!gameStarted) {
-    lastHoveredCoords = { x, y };
-    const shipLength = 5
-    if (isValidPlacement({ start: { x, y }, shipLength, direction })) {
-      for (let i = 0; i < shipLength; ++i) {
+  if (!gameStarted && Ships.curShip) {
+    lastHoveredCoords = { x, y }
+    if (isValidPlacement({ start: { x, y }, shipLength: Ships.curShip.len, direction })) {
+      for (let i = 0; i < Ships.curShip.len; ++i) {
         if (direction === Direction.DOWN) board.value[y + i][x] = CellValue.HOVER
         else board.value[y][x + i] = CellValue.HOVER
       }
@@ -93,10 +138,9 @@ function onHover({ x, y }: Coordinate) {
 }
 
 function onHoverLeave({ x, y }: Coordinate) {
-  if (!gameStarted) {
-    const shipLength = 5
-    if (isValidPlacement({ start: { x, y }, shipLength, direction })) {
-      for (let i = 0; i < shipLength; ++i) {
+  if (!gameStarted && Ships.curShip) {
+    if (isValidPlacement({ start: { x, y }, shipLength: Ships.curShip.len, direction })) {
+      for (let i = 0; i < Ships.curShip.len; ++i) {
         if (direction === Direction.DOWN) board.value[y + i][x] = CellValue.EMPTY
         else board.value[y][x + i] = CellValue.EMPTY
       }
@@ -110,8 +154,8 @@ function onHoverLeave({ x, y }: Coordinate) {
   <table class="board" @mouseover="onBoard = true" @mouseleave="onBoard = false">
     <tr v-for="(row, y) in board" :key="y">
       <td class="table-success" v-for="(col, x) in row" :key="x">
-        <div :class="{ hovered: board[y][x] === CellValue.HOVER }" @click="onCellClick({ x, y })"
-          @mouseover="onHover({ x, y })" @mouseleave="onHoverLeave({ x, y })" class="cell">
+        <div :class="{ hovered: board[y][x] === CellValue.HOVER, ship: board[y][x] === CellValue.SHIP }"
+          @click="placeShip({ x, y })" @mouseover="onHover({ x, y })" @mouseleave="onHoverLeave({ x, y })" class="cell">
           {{ board[y][x] }}
         </div>
       </td>
@@ -150,5 +194,9 @@ td {
 
 .hovered {
   background-color: blue;
+}
+
+.ship {
+  background-color: red;
 }
 </style>
